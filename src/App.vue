5ce7855f36bdcdf2d39c8d5ca3ce1e8fbed6c7b5
jessicacaron <77312057+jessicacaron@users.noreply.github.com>
2022-02-12 19:56:27 -0700

    initial

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+  <h2>{{ title }}</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      title: 'Hello World'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
